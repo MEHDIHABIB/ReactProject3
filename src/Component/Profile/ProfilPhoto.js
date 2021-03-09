582492b48f64d7450bb047e78007cd74d1badf89
MEHDIHABIB <78062259+MEHDIHABIB@users.noreply.github.com>
2021-03-09 00:30:55 +0000

    first commit

---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const ProfilPhoto = () => {
+    return (
+        <div>
+            <img style={{width:"300px", height:"300px"}} src="https://img2.freepng.fr/20180405/cce/kisspng-computer-icons-icon-design-avatar-5ac66832d44b93.1745920615229522428696.jpg" alt='myImage' />
+        </div>
+    
+    )
+}
+
+export default ProfilPhoto
